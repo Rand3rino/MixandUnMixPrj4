5a601524d0cd74d227b608072c91e94c4abe06cc
Randy Nguyen <nguyeran@mail.gvsu.edu>
2018-03-28 22:28:24 -0400

    unmix

--- src/Project4/UnMix.java
+++ src/Project4/UnMix.java
@@ -2,4 +2,28 @@ package Project4;
 
 public class UnMix {
 
+	/** FIXME */
+	private LinkedList message = new LinkedList();
+	
+	/** This is the input message */
+	private static String userMessage;
+
+
+
+	public void main(String [] args) {
+		UnMix.userMessage = "";
+
+		for (int i = 0; i < args.length; i++) {
+			UnMix.userMessage += args[i] + "";
+		}
+		
+		System.out.print ("Message:\n");
+		for (int i = 0; i < userMessage.length(); i++) 
+			System.out.format ("%3d", i);
+		System.out.format ("\n");
+		for (char c : userMessage.toCharArray()) 
+			System.out.format("%3c",c);
+		System.out.format ("\n");
+
+	}
 }
