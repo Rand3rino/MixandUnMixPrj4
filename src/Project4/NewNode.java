06d51e4b8300821dc86857f755f8d44a6f0ecc98
Randy Nguyen <nguyeran@mail.gvsu.edu>
2018-03-28 21:32:23 -0400

    node and new node

---
+++ src/Project4/NewNode.java
@@ -0,0 +1,10 @@
+package Project4;
+
+public class NewNode {
+	
+		private Node top;   // Node references the top of each clipboard
+		private int clipboardNumber;
+		private NewNode next;
+
+
+}
--- src/Project4/Node.java
+++ src/Project4/Node.java
@@ -1,5 +1,29 @@
 package Project4;
 
 public class Node {
+	private String data;
+	private Node next;
 
+	public Node(String data, Node next) {
+		this.data = data;
+		this.next = next;
+	}
+	public Node() {
+	}
+
+	public String getData() {
+		return data;
+	}
+	
+	public void setData(String data) {
+		this.data = data;
+	}
+	
+	public void setNext(Node next) {
+		this.next = next;
+	}
+	
+	public Node getNext() {
+		return next;
+	}
 }
