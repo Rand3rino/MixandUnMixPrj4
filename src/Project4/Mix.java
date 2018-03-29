8ecc55a2ca1579a80e8f87d9438c48fd369bc302
Randy Nguyen <nguyeran@mail.gvsu.edu>
2018-03-28 22:06:58 -0400

    main

--- src/Project4/Mix.java
+++ src/Project4/Mix.java
@@ -4,4 +4,14 @@ public class Mix {
 	//FIXME
 	//FIXME
 	//FIXME
+	private message = new LinkedList;
+}
+
+
+public void main(String [] args) {
+	Mix.userMessage = "";
+	
+	for (int i = 0; i < args.length; i++) {
+		Mix.userMessage += arg[i] + "";
+	}
 }
