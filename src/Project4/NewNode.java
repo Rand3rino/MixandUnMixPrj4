98cd9e34c363e7b619ddb2a2d4c63989194130f2
Randy Nguyen <nguyeran@mail.gvsu.edu>
2018-03-28 21:42:04 -0400

    instance variables

--- src/Project4/NewNode.java
+++ src/Project4/NewNode.java
@@ -3,8 +3,10 @@ package Project4;
 public class NewNode {
 	
 		private Node top;   // Node references the top of each clipboard
+		private String data;
 		private int clipboardNumber;
 		private NewNode next;
+		private Node List;
 
 
 }
